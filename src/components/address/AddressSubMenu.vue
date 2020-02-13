e7225f288727ba533c71fa2ad4b015d564dfe606
jeonelfin <jeonregina11@gmail.com>
2020-02-13 17:14:28 +0900

    css2

--- src/components/address/AddressSubMenu.vue
+++ src/components/address/AddressSubMenu.vue
@@ -2,12 +2,12 @@
     <div class="subMenu">
         <ul class="nav nav-pills flex-column">
 
-            <div style="font-weight: bold; margin: 8%; font-size: 20px"><h4>공용주소록</h4></div>
+            <div style="font-weight: bold; margin: 8%; font-size: 20px">공용주소록</div>
             <router-link to="/mainEmployAddress">
-                <li class="small-menu">내부주소록</li>
+                <li class="list-group-item d-flex justify-content-between align-items-center libgclr">내부주소록</li>
             </router-link>
             <router-link to="/mainOutAddress">
-                <li class="small-menu">외부주소록</li>
+                <li class="list-group-item d-flex justify-content-between align-items-center libgclr">외부주소록</li>
             </router-link>
         </ul>
     </div>
